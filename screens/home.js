100754b7eeddcab8c4668baa9c84255e26424953
puldyastin <puldyastin@gmail.com>
2021-12-08 18:28:22 +0800

    finalized cards

--- screens/home.js
+++ screens/home.js
@@ -9,15 +9,13 @@ import {
   Center,
   NativeBaseProvider,
   VStack,
-  Image,
+  Link,
   ScrollView,
   Code
 } from "native-base"
-import start from '../assets/cmd.png';
-import create from '../assets/create.png';
 export function Example() {
   return (
-      <ScrollView>
+      <ScrollView  showsVerticalScrollIndicator={false}>
     <VStack space={4} alignItems="center">
 
     <Pressable
@@ -43,7 +41,7 @@ export function Example() {
         </Text>
         <Flex>
           <Text mt="2" fontSize={12} fontWeight="medium" color="cyan.400">
-            Read More
+            
           </Text>
         </Flex>
       </Box>
@@ -69,10 +67,24 @@ export function Example() {
         <Text mt="2" fontSize={14} color="cyan.100">
       If you are already experienced with React and JavaScript tooling and want to dive right in and figure things out as you go, this is the quickest way to get started:
         </Text>
-       <Image source={start}  style={{ width: 'auto', height: 120 }}/>
+       {/* <Image source={start}  style={{ width: 'auto', height: 120 }}/> */}
+        <Code bg="black">
+           <Text p="1" fontSize={14} color="gray.500">
+              # Install the command line tools
+           </Text>
+           <Code bg="gray.400">
+            $ npm install --global expo-cli
+           </Code>
+           <Text mt="2" p="1" fontSize={14} color="gray.500">
+              # Create a new project
+           </Text>
+           <Code bg="gray.400">
+            $ expo init my-app
+           </Code>
+        </Code>
         <Flex>
           <Text mt="2" fontSize={12} fontWeight="medium" color="cyan.400">
-            Read More
+            
           </Text>
         </Flex>
       </Box>
@@ -98,18 +110,23 @@ export function Example() {
         <Text mt="2" fontSize={14} color="cyan.100">
         we should have Expo CLI installed on our development machine and the Expo Go app on an iOS or Android physical device or emulator
         </Text>
-       {/* <Image source={create}  style={{ width: 'auto', height: 100 }}/> */}
         <Code bg="black">
-           <Text mb="2" p="1" fontSize={14} color="gray.500">
+           <Text p="1" fontSize={14} color="gray.500">
               # Create a project named my-app. Select the "blank" template when promted
            </Text>
            <Code bg="gray.400">
             $ expo init my-app
            </Code>
+           <Text mt="2" p="1" fontSize={14} color="gray.500">
+              # Navigate to the project directory
+           </Text>
+           <Code bg="gray.400">
+            $ cd my-app
+           </Code>
         </Code>
         <Flex>
           <Text mt="2" fontSize={12} fontWeight="medium" color="cyan.400">
-            Read More
+            
           </Text>
         </Flex>
       </Box>
@@ -138,15 +155,37 @@ export function Example() {
         <Text mt="2" fontSize={14} color="cyan.100">
        When you run expo start (or npm start), Expo CLI starts Metro Bundler, which is an HTTP server that compiles the JavaScript code of our app using Babel and serves it to the Expo app. It also pops up Expo Dev Tools, a graphical interface for Expo CLI.
         </Text>
-       <Image source={create}  style={{ width: 'auto', height: 100 }}/>
+       <Code bg="black">
+           {/* <Text p="1" fontSize={14} color="gray.500">
+              # Create a project named my-app. Select the "blank" template when promted
+           </Text> */}
+           <Code bg="gray.400">
+            $ expo start
+           </Code>
+          
+        </Code>
         <Flex>
           <Text mt="2" fontSize={12} fontWeight="medium" color="cyan.400">
-            Read More
+            
           </Text>
         </Flex>
       </Box>
     </Pressable>
 
+        <Text mt="2" fontSize={14} fontWeight="medium" color="gray.500">
+           Join the Community
+          </Text>
+        <Link
+                _text={{
+                color: "indigo.500",
+                fontWeight: "medium",
+                fontSize: "sm",
+                }}
+                href="#"
+                onClick={navigateSignUp}
+            >
+                Join the Expo forums
+            </Link>
     </VStack>
     </ScrollView>
   )
